df153d61be99b64cd3b75dc41a501b1310c44f31
Andres Sierra <andressierra@192.168.1.4>
2024-06-06 06:48:34 -0500

    Subiendo web de don Gustavo

---
+++ post/js/scripts.js
@@ -0,0 +1,16 @@
+/*!
+* Start Bootstrap - Blog Post v5.0.9 (https://startbootstrap.com/template/blog-post)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-blog-post/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
+
+
+<script>
+    document.getElementById('button-search').addEventListener('click', function() {
+        var searchTerm = document.getElementById('search-term').value;
+        // Aquí puedes hacer algo con el término de búsqueda, como enviarlo al servidor o mostrar resultados en la página
+        console.log("Se realizó una búsqueda con el término: " + searchTerm);
+    });
+</script>
